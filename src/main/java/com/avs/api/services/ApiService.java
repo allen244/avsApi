df0e766c7fa99309b21135972271e93f62fc57b1
asmaldone <alsmaldone@yahoo.com>
2018-01-13 13:48:25 -0500

    AVS API

--- src/main/java/com/avs/api/services/ApiService.java
+++ src/main/java/com/avs/api/services/ApiService.java
@@ -1,10 +1,14 @@
 package com.avs.api.services;
 
 import com.avs.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
